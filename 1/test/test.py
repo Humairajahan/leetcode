8decbe23d9c788d259f21adb7e28396bc09356b1
Humairajahan <humairaazim45@gmail.com>
2023-07-19 15:53:00 +0600

    deleted unnecessary folder

--- 1/test/test.py
+++
@@ -1,7 +0,0 @@
-from romanToInt import Solution
-from unittest import TestCase
-
-class TestRomanToInt(TestCase):
-    def test_output_is_str(self):
-        s = Solution("III").romanToInt()
-        self.assertIsInstance(s, str)
