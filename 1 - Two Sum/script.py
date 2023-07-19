bebb0e6f089024d8931bcc771e1fcdc814c2659c
Humairajahan <humairaazim45@gmail.com>
2023-07-19 15:41:35 +0600

    fixed bug: returning position

--- 1 - Two Sum/script.py
+++ 1 - Two Sum/script.py
@@ -4,12 +4,20 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         outcome = {}
-        for num in range(len(nums)):
-            complement = target - nums[num]
-            if nums[num] not in outcome:
-                outcome[complement] = nums[num]
-            return [nums[num], complement]
-    
+        for idx in range(len(nums)):
+            complement = target - nums[idx]
+            if complement in outcome:
+                return [outcome[complement], idx]
+            outcome[nums[idx]] = idx
 
-s = Solution().twoSum(nums = [2,7,11,15], target = 9)
-print(s)
+
+s = Solution()
+
+test_case1 = s.twoSum(nums=[2, 7, 11, 15], target=9)
+print(test_case1)
+
+test_case2 = s.twoSum(nums = [3,2,4], target = 6)
+print(test_case2)
+
+test_case3 = s.twoSum(nums = [3,3], target = 6)
+print(test_case3)
