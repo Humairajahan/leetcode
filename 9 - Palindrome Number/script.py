09e2aa25a5ec8b1baaee2e06c2f6d9c15bd4d5af
Humairajahan <humairaazim45@gmail.com>
2023-07-23 10:04:04 +0600

    9 : avoiding str-conversion|runtime increased to 77ms

--- 9 - Palindrome Number/script.py
+++ 9 - Palindrome Number/script.py
@@ -1,4 +1,4 @@
-### Naive / Brute force Approach : Converting to string
+### Naive / Brute force Approach I : Converting to string
 
 
 class Solution:
@@ -6,6 +6,22 @@ class Solution:
         l2r = str(x)
         r2l = l2r[::-1]
         return l2r == r2l
+
+
+### Naive / Brute force Approach II : Without converting to string
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reversed_num = 0
+        original_num = x
+        if x < 0:
+            return False
+        while x > 0:
+            q, r = divmod(x, 10)
+            reversed_num = reversed_num * 10 + r
+            x = q
+        return reversed_num == original_num
 
 
 s = Solution()
