5b7cc067f07d16f3bbcb2a991a492c294f09d2e2
Humairajahan <humairaazim45@gmail.com>
2024-02-14 17:19:35 +0600

    IsSubsequence: fast-slow

---
+++ LeetCode 75/Two Pointers/392_IsSubsequence.py
@@ -0,0 +1,35 @@
+# 392. Is Subsequence
+
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+# Example 1:
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# Example 2:
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        slow = 0
+        s = list(s)
+        t = list(t)
+
+        if len(s) > len(t):
+            return False
+        if len(s) == 0:
+            return True
+
+        for fast in range(len(t)):
+            if t[fast] == s[slow]:
+                slow += 1
+
+            if slow == len(s):
+                return True
+
+        return False
