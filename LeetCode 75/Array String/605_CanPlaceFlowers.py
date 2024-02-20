e0f1f2a7cd67871d7f849ac6abfe6e42243daa80
Humairajahan <humairaazim45@gmail.com>
2024-02-20 20:19:38 +0600

    CanPlaceFlowers: greedy

---
+++ LeetCode 75/Array String/605_CanPlaceFlowers.py
@@ -0,0 +1,32 @@
+# 605. Can Place Flowers
+
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+# Example 1:
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+
+# Example 2:
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
+
+
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowers = 0
+        num_beds = len(flowerbed)
+        for i in range(num_beds - 1):
+            if (
+                (i == 0 or flowerbed[i - 1] == 0)
+                and flowerbed[i] == 0
+                and (i == num_beds - 1 or flowerbed[i + 1] == 0)
+            ):
+                flowerbed[i] = 1
+                flowers += 1
+        return flowers >= n
