929fe22ac84060a40c1be09b5e43e50638cdc377
Humairajahan <humairaazim45@gmail.com>
2024-03-21 13:58:24 +0600

    ConvertBinaryNumberinaLinkedListtoInteger: Array

---
+++ Daily Challenges/1290_ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -0,0 +1,55 @@
+# 1290. Convert Binary Number in a Linked List to Integer
+
+# Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+# Return the decimal value of the number in the linked list.
+
+# The most significant bit is at the head of the linked list.
+
+
+# Example 1
+# ---------
+# Input: head = [1,0,1]
+# Output: 5
+# Explanation: (101) in base 2 = (5) in base 10
+
+# Example 2
+# ---------
+# Input: head = [0]
+# Output: 0
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Array
+# Time complexity O(n)
+# Space complexity O(n)
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        binary = []
+        while head:
+            binary.append(head.val)
+            head = head.next
+
+        length = len(binary)
+        bin_num = 0
+        for i in range(length):
+            bin_num += binary[i] * 2 ** (length - 1 - i)
+        return bin_num
+
+
+node1 = ListNode(1)
+node2 = ListNode(0)
+node3 = ListNode(1)
+
+node1.next = node2
+node2.next = node3
+
+
+print(Solution().getDecimalValue(node1))
