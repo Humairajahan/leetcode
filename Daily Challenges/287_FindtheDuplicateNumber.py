1e37ac058d972213a2cb96b183d7a317526c5cb8
Humairajahan <humairaazim45@gmail.com>
2024-03-24 11:30:59 +0600

    FindtheDuplicateNumber: Linked List Approach

--- Daily Challenges/287_FindtheDuplicateNumber.py
+++ Daily Challenges/287_FindtheDuplicateNumber.py
@@ -55,6 +55,28 @@ class Solution:
         return nums[0]
 
 
+# Linked List Approach
+# Time complexity O(n)
+# Space complexity O(1)
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+
+
 print(Solution().findDuplicate(nums=[1, 3, 4, 2, 2]))
 
 print(Solution().findDuplicate(nums=[3, 1, 3, 4, 2]))
